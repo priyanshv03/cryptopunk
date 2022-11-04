3ceaafb37affbe2bf18e3e01b5c1f9afb1d12f7c
jagdish <pkjagdish4501@gmail.com>
2022-11-04 10:32:36 +0530

    login signup created

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "JK_123",
+  PASSWORD: "EtEB)6PsbZrpA-o8",
+  DB: "test",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
