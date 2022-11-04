37b19ded9d0522bb588de7e5bd765a1b4bdf989b
jagdish <pkjagdish4501@gmail.com>
2022-11-04 18:05:04 +0530

    some error from login signup feature are resolve

--- backend/server.js
+++ backend/server.js
@@ -48,19 +48,19 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user",
-  });
+// function initial() {
+//   Role.create({
+//     id: 1,
+//     name: "user",
+//   });
 
-  Role.create({
-    id: 2,
-    name: "moderator",
-  });
+//   Role.create({
+//     id: 2,
+//     name: "moderator",
+//   });
 
-  Role.create({
-    id: 3,
-    name: "admin",
-  });
-}
+//   Role.create({
+//     id: 3,
+//     name: "admin",
+//   });
+// }
